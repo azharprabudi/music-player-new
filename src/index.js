63f6fe37c76c3a2bd72f1b818d540f0ac2abc941
azharprabudi <azharprabui@gmail.com>
2019-04-09 23:24:09 +0700

    implement redux at homepage

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import Homepage from "@pages/homepage";
+import makeStore from "@utils/redux";
 
-ReactDOM.render(<Homepage />, document.getElementById("music-player"));
+console.log(makeStore);
+
+const App = () => (
+  <Provider store={makeStore()}>
+    <Homepage />
+  </Provider>
+);
+
+ReactDOM.render(<App />, document.getElementById("music-player"));

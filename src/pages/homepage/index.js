372f698b69910f40c7b4d21aa1f8e8680941fb9a
Azhar Prabudi <azharprabui@gmail.com>
2019-05-03 08:38:22 +0700

    create prototype song page

--- src/pages/homepage/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Homepage = () => (
-  <div className="container">
-    <h1>helo</h1>
-  </div>
-);
-
-export default Homepage;
---
+++ src/pages/song/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SongPage = () => (
+  <div className="container">
+    <h1>Song page</h1>
+  </div>
+);
+
+export default SongPage;

1ffb91096eb8119d78bcfa86a4086c1a91e089be
azharprabudi <azharprabui@gmail.com>
2019-05-03 19:40:41 +0700

    change url music player

--- webpack/utils/config.js
+++ webpack/utils/config.js
@@ -2,5 +2,7 @@ module.exports = {
   DEV_PORT: 3000,
   PRODUCTION: "production",
   DEVELOPMENT: "development",
-  BASE_URL: "https://api.musixmatch.com/ws/1.1/"
+  BASE_URL:
+    "https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/",
+  API_KEY: ""
 };

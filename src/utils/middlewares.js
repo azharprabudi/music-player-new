3f4f19bd2229fdb6370f8fcfd45f35161e55986f
Azhar Prabudi <azharprabui@gmail.com>
2019-05-04 21:20:51 +0700

    combine all epic

--- src/utils/middlewares.js
+++ src/utils/middlewares.js
@@ -1,13 +1,13 @@
 import { applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-import { createEpicMiddleware } from "redux-observable";
-import { fetchPopularSongs$ } from "@epics/song";
+import { createEpicMiddleware, combineEpics } from "redux-observable";
+import { fetchPopularSongs$, searchSong$ } from "@epics/song";
 
-const _epics = [fetchPopularSongs$];
+const _epics = combineEpics(fetchPopularSongs$, searchSong$);
 const epicMiddleware = createEpicMiddleware();
 const _middlewares = applyMiddleware(epicMiddleware);
 
-export const runEpicMiddleware = () => epicMiddleware.run(..._epics);
+export const runEpicMiddleware = () => epicMiddleware.run(_epics);
 export default (process.env.MUSIC_PLAYER_NODE_ENV === "development"
   ? composeWithDevTools(_middlewares)
   : _middlewares);
